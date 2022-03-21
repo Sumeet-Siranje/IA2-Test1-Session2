34eb8ae792f07f3fe9d4c3c9210688b04c656dd2
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-03-21 06:26:52 +0000

    p5final.c_done

--- p5final.c
+++ p5final.c
@@ -1,43 +1,29 @@
 
-#include <stdio.h>
-int input()
-{
-  int a;
-  printf("Enter the number\n");
-  scanf("%d%d",&a);
-  return a;
+#include<stdio.h>
+int input(){
+  int n;
+  printf("enter the number\n");
+  scanf("%d",&n);
+  return n;
 }
-
-int find_gcd(int a, int b)
-{
-  int gcd,t;
-  if( a >b)
-  {
-    t = a;
-    a = b;
-    b = t;
-  }
-  if ( b%a == 0)
-    return a;
-  for(int i=2; i<a/2; i++)
-  {
-    if ( a%i==0 && b%i==0)
-      gcd = i;
+int gcd(int a,int b){
+  int gcd=1;
+  for(int i=2;i<=a && i<=b;i++){
+    if(a%i==0 && b%i==0){
+      gcd=i;
+    }
   }
   return gcd;
+  }
+void output(int a,int b,int gcd){
+  printf("%d is GCD of %d and %d\n",gcd,a,b);
 }
-
-void output(int a, int b, int gcd)
-{
-  printf("The gcd %d and %d is %d\n",a,b,gcd);
-}
-
 int main()
 {
-  int a, b, gcd;
-  a = input();
-  b = input();
-  gcd = find_gcd(a,b);
-  output(a,b,gcd);
+  int a,b,result;
+  a=input();
+  b=input();
+  result=gcd(a,b);
+  output(a,b,result);
   return 0;
 }
